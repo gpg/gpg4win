61669383234b22d37c08dd0d8cbaedfcb8541dae
Jan-Oliver Wagner <jan@intevation.de>
2006-02-17 20:59:37 +0000

    Fixed targets "install" and "tar". Added target "online" which calls the rsync.

--- trunk/doc/website/buildhtml.mk
+++ trunk/doc/website/buildhtml.mk
@@ -15,9 +15,13 @@ TARGETS = $(patsubst %.htm4,%.html,$(wildcard *.htm4))
 SUBDIRS = 
 
 # Installation configuration
-INSTALL_DIR = /tmp/kolabkonsortium-www
-ADD_INST_TYPES = .css .pdf
+INSTALL_DIR = /tmp/gpg4win-www
+ADD_INST_TYPES = .css
 ADD_INST_DIRS = pix
+
+USER=$(shell grep "svn+ssh://" .svn/entries | sed -e "s/.*svn+ssh:\/\///g" | sed -e "s/@.*//g")
+
+CURDIR=$(shell pwd)
 
 .SUFFIXES: .html .htm4
 
@@ -33,16 +37,18 @@ subdirs: $(SUBDIRS)
 	  $(MAKE) -C $$dir SUBDIRS="" ; \
 	done
 
+online: all
+	echo "Going to put current contents online for www.gpg4win.org ..."
+	rsync -urvP --exclude='.svn' *.css *.html *.php pix \
+      $(USER)@wald.intevation.org:/gpg4win/htdocs/
+
 install: all
 	mkdir -p $(INSTALL_DIR) ;\
 	cp -uf $(TARGETS) $(INSTALL_DIR) ;\
 	cp -uf *$(ADD_INST_TYPES) $(INSTALL_DIR)
 	cp -urf *$(ADD_INST_DIRS) $(INSTALL_DIR)
+	find $(INSTALL_DIR) -name ".svn" | xargs rm -rf
 
-tar: all
-	rm -f .TAR-FILE-LIST ; \
-	for i in $(ADD_INST_DIRS) ; do echo "./$$i" >>.TAR-FILE-LIST ; done ; \
-	for i in .html $(ADD_INST_TYPES) ; do \
-	  find . -name "*$$i" >>.TAR-FILE-LIST ; \
-	done ; \
-	tar -czv -f www.kolab-konsortium.tar.gz -T .TAR-FILE-LIST
+tar: install
+	echo $(CURDIR)
+	( cd $(INSTALL_DIR) ; tar -czv -f $(CURDIR)/www.gpg4win.org.tar.gz . )
