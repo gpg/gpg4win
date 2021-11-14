578ea4764731fb523da61e101f0138799bca18d2
Werner Koch <wk@gnupg.org>
2021-11-14 18:40:10 +0100

    appimage: Add a --devel option to the docker build script.
    --

--- docker/run-appimage-build.sh
+++ docker/run-appimage-build.sh
@@ -2,15 +2,25 @@
 
 set -e
 
+if [ "$1" = "--devel" ]; then
+    devmode=
+else
+    devmode="/build-appimage.sh"
+fi
+
 sourcedir=$(cd $(dirname $0)/..; pwd)
 
-buildroot=$(mktemp -d --tmpdir appimage-kleopatra.XXXXXXXXXX)
+if [ -n "$devmode" ]; then
+    buildroot="/tmp/appimage-gnupg.$(id -un).d"
+else
+    buildroot=$(mktemp -d --tmpdir appimage-gnupg.XXXXXXXXXX)
+fi
 echo Using ${buildroot}
+
 
 docker run -it --rm --user "$(id -u):$(id -g)" \
     --volume ${sourcedir}:/src \
     --volume ${buildroot}:/build \
-    g10-build-appimage-kleopatra:centos7 \
-    /build-appimage.sh
+    g10-build-appimage-kleopatra:centos7 $devmode
 
 echo "You can find the AppImage in ${buildroot} (if the build succeeded)."
