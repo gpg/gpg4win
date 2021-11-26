6e0c1d0b8f0c66ff96d3fa1e7415147baa9cab7f
Andre Heinecke <aheinecke@gnupg.org>
2021-11-26 15:30:36 +0100

    appimage: Enable GnuPG VS-D Version
    * Makefile.am: Add patches.
    * patches-appimage/kleopatra, patches-appimage/libkleo:
    Add patches to enable version check.
    * docker/appimage/build-appimage.sh: Install VERSION files.

--- docker/appimage/build-appimage.sh
+++ docker/appimage/build-appimage.sh
@@ -95,6 +95,7 @@ if [ $GNUPG_BUILD_VSD = yes ]; then
     OUTPUT=gnupg-vs-desktop-${myversion}-x86_64.AppImage
     echo "Packaging GnuPG VS-Desktop Appimage: $myversion"
     echo $myversion >/build/AppDir/GnuPG-VS-Desktop-VERSION
+    cp /src/src/gnupg-vsd/Standard/VERSION* /build/AppDir/usr/
 else
     myversion=$(ls /src/packages/gnupg-2.*tar.* \
                     | sed -n 's,.*/gnupg-\(2.*\).tar.*,\1,p')
