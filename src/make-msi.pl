fc13acb761bee5ab7fe4b706ae6aef0531b7a157
Werner Koch <wk@gnupg.org>
2024-03-06 14:14:54 +0100

    Fix filename for shield.ico.
    --
    
    GnuPG-bug-id: 7026

--- src/make-msi.pl
+++ src/make-msi.pl
@@ -2079,8 +2079,8 @@ print <<EOF;
     <!-- Turn on logging
         <Property Id="MsiLogging" Value="gnupg-desktop"/>
     -->
-    <Icon Id="shield.ico" SourceFile="icons/shield.ico"/>
-    <Property Id="ARPPRODUCTICON" Value="icons/shield.ico"/>
+    <Icon Id="shield.ico" SourceFile="shield.ico"/>
+    <Property Id="ARPPRODUCTICON" Value="shield.ico"/>
 
     <WixVariable Id="WixUIBannerBmp" Value="header.bmp" />
     <WixVariable Id="WixUIDialogBmp" Value="dialog.bmp" />
