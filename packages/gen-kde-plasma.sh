a291b429a76bff4f5b87b2f4337cd1f70813c4d2
Ingo Klöcker <dev@ingo-kloecker.de>
2026-06-25 11:51:04 +0200

    appimage: Add kde-cli-tools package
    --
    
    For now we only build the kioclient component which include kde-open.
    
    GnuPG-bug-id: 8308

---
+++ packages/gen-kde-plasma.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Copyright (C) 2016 Intevation GmbH
+# Copyright (C) 2021 g10 Code GmbH
+#
+# Software engineering by Ingo Klöcker <dev@ingo-kloecker.de>
+#
+# This file is part of GPG4Win.
+#
+# GPG4Win is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# GPG4Win is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-2.0+
+
+# Grab the version information for packages released with a KDE Plasma release
+# and generate a text block that can be copy and pasted into packages.current.
+
+set -e
+
+if [ -z "$1" ]; then
+    echo "Usage $0 <Version> > snippet"
+    exit 1
+fi
+
+PACKAGES="kde-cli-tools"
+
+fullversion=$1
+case ${fullversion} in
+    *.*.*)
+        majorversion=${fullversion%.*}
+        ;;
+    *.*)
+        majorversion=${fullversion}
+        fullversion=${majorversion}.0
+        echo "Using full version ${fullversion}"
+        ;;
+    *)
+        echo "Invalid version ${fullversion}"
+        exit 1
+        ;;
+esac
+
+curdate=$(date +%Y-%m-%d)
+
+KEYRING=$(dirname $0)/kde-release-keys.gpg
+
+server=https://download.kde.org/stable/plasma
+echo "server ${server}"
+
+tmpdir=$(mktemp -d -t gen-kde-plasma.XXXXXXXXXX)
+
+for package in $PACKAGES; do
+    # Download packages over https now and verify that the signature matches
+    tarfile="${package}-${fullversion}.tar.xz"
+    tarfileurl="${server}/${fullversion}/${tarfile}"
+    if ! curl -L --silent --show-error --fail "${tarfileurl}" > "$tmpdir/${tarfile}"; then
+        echo "Downloading ${tarfileurl} failed"
+        exit 1
+    fi
+    sigfile="${tarfile}.sig"
+    sigfileurl="${tarfileurl}.sig"
+    if ! curl -L --silent --show-error --fail "${sigfileurl}" > "$tmpdir/${sigfile}"; then
+        echo "Downloading ${sigfileurl} failed"
+        exit 1
+    fi
+    # Check the signature
+    if ! gpgv --keyring "$KEYRING" "$tmpdir/${sigfile}" "$tmpdir/${tarfile}"; then
+        echo "Signature for $tmpdir/${tarfile} is not valid!"
+        exit 1
+    fi
+
+    sha2=$(sha256sum $tmpdir/${tarfile} | cut -d ' ' -f 1)
+
+    cat > ${tmpdir}/snippet <<EOF
+# ${package}
+# last changed: ${curdate}
+# by: $(echo ${EMAIL-$USER}|sed 's/@.*//')
+# verified: PGP Signed by ./kde-release-keys.gpg (created by gen-kde-plasma.sh)
+file ${fullversion}/${tarfile}
+chk $sha2
+
+EOF
+
+perl -i -p0e "s@# ${package}\n# last changed:.*?\n# by:.*?\n# verified:.*?\nfile.*?\nchk.*?\n@'`cat ${tmpdir}/snippet`
+'@se" packages.list
+done
+
+rm -r $tmpdir
