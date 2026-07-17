4f2f122ab77ce8014cdec4633aaa70289c9c162c
Thomas Friedrichsmeier <thomas.friedrichsmeier@gnupg.com>
2026-07-17 08:07:52 +0200

    Explicitly list all remaining homebrew paths

--- src/mac/build-macimage.sh
+++ src/mac/build-macimage.sh
@@ -116,8 +116,10 @@ fi
 # The actual build
 cd ${BUILDROOT}
 PATH=/opt/homebrew/opt/bison/bin:$PATH
-#PATH=/opt/homebrew/opt/gnu-tar/libexec/gnubin/:$PATH
-#PATH=/opt/homebrew/opt/coreutils/libexec/gnubin/:$PATH
+PATH=/opt/homebrew/opt/pkgconf/bin:$PATH
+#PATH=/opt/homebrew/opt/meson/bin:$PATH
+PATH=/opt/homebrew/opt/gettext/bin:$PATH # configure.ac checks for msgfmt/msgmerge, although we build gettext, ourselves?!
+PATH=/opt/homebrew/opt/cmake/bin:$PATH
 PATH=/Applications/Xcode.app/Contents/Developer/usr/bin:$PATH
 ${SRCDIR}/configure --enable-macimage --with-playground=${BUILDROOT}
 # Nuke the AppDir to make sure we get everything nice and clean
